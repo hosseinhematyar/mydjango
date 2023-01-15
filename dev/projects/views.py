693c549af00eec8e4ab188663bcf7cc0cdb7031d
Hossein Hematyar <hosseinhematyar@gmail.com>
2023-01-15 23:40:50 +0330

    Back to original django template urls

--- dev/projects/views.py
+++ dev/projects/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
-
-
-# from django.http import HttpResponse
+from django.http import HttpResponse
 
 
 def projects(request):
     # return HttpResponse('Here are our projects')
-    return render(request, 'projects/projects.html')
+    return render(request, 'projects.html')
 
 
 def project(request, pk):
     # return HttpResponse('Single Project' + ' ' + str(pk))
-    return render(request, 'projects/single-project.html')
+    return render(request, 'single-project.html')
