c566c02992e934036b9e66369f09645c00b4ca4c
Hossein Hematyar <hosseinhematyar@gmail.com>
2023-01-15 17:24:39 +0330

    Added templates in separate template folder for apps

--- dev/projects/views.py
+++ dev/projects/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+
+# from django.http import HttpResponse
 
 
 def projects(request):
-    return HttpResponse('Here are our projects')
+    # return HttpResponse('Here are our projects')
+    return render(request, 'projects/projects.html')
 
 
 def project(request, pk):
-    return HttpResponse('Single Project' + ' ' + str(pk))
-
+    # return HttpResponse('Single Project' + ' ' + str(pk))
+    return render(request, 'projects/single-project.html')
