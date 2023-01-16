a45666cf2d740771b03cba71fc44e33db508af1b
Hossein Hematyar <hosseinhematyar@gmail.com>
2023-01-16 20:26:34 +0330

    Create dynamic name for pages Migrate DB Create SuperUser in DB

--- dev/projects/views.py
+++ dev/projects/views.py
@@ -1,12 +1,39 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# from django.http import HttpResponse
+
+
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce website',
+        'description': 'Fully functional ecommerce website',
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio website',
+        'description': 'This was a project where I built out my portfolio',
+    },
+    {
+        'id': '3',
+        'title': 'Social website',
+        'description': 'Awesome open source project I am still working',
+    }
+]
 
 
 def projects(request):
     # return HttpResponse('Here are our projects')
-    return render(request, 'projects.html')
+    page = 'Projects List'
+    number = 10
+    context = {'page': page, 'number': number, 'projects': projectsList}
+    return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
     # return HttpResponse('Single Project' + ' ' + str(pk))
-    return render(request, 'single-project.html')
+    projectObj = None
+    for i in projectsList:
+        if i['id'] == pk:
+            projectObj = i
+    return render(request, 'projects/single-project.html', {'project': projectObj})
